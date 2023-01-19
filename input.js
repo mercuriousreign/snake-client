6a78eacb83e267207e15d15b5b49f021fb000342
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-19 08:34:08 -0500

    allowed setupInput to reference connection, also implemented program receiveing wasd input

--- input.js
+++ input.js
@@ -1,7 +1,9 @@
 // setup interface to handle user input from stdin
 const stdin = process.stdin;
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -10,7 +12,22 @@ const setupInput = function () {
 
 
 const handleUserInput = function (key) {
-  console.log(key);
+  //console.log(key);
+  if (key==='w') {
+    console.log(key);
+  }
+
+  if (key==='a') {
+    console.log(key);
+  }
+
+  if (key==='s') {
+    console.log(key);
+  }
+
+  if (key==='d') {
+    console.log(key);
+  }
   //Exit process if Ctrl+C is the input
 
   if (key === '\u0003') {
